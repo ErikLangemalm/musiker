91c939e4dba862d0876c04ed0b828fe850a9b6a5
Erik Langemalm <82160523+ErikLangemalm@users.noreply.github.com>
2023-10-23 19:38:25 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
+import Musiker from "./classes";
+import Band from ".classes"
+import fs from "fs";
 
+const data = fs.readFileSync('data.json');
+const jsonData = JSON.parse(data);
+
+let band = new Band("1999");
