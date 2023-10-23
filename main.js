f73b933fb65fd1ebdf78ca8382e5e6d735ae7d75
Erik Langemalm <82160523+ErikLangemalm@users.noreply.github.com>
2023-10-23 16:19:06 +0200

    moved classes to separate file

--- main.js
+++ main.js
@@ -1,16 +1,1 @@
 
-class Musiker {
-  constructor() {
-
-  }
-}
-
-class Band {
-  constructor(datum) {
-    this.info = "";
-    this.startDate = datum;
-    this.expDate = "";
-    this.members = [];
-    this.exMembers = []
-  }
-}
